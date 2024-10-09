36a77680bc5bb0c716365ba84a1c076398c921fa
Larry Jones <mrwizard82d1@gmail.com>
2024-10-09 16:53:35 -0500

    Begin creating view in PyCharm IDE

--- weatherApp/meteo/views.py
+++ weatherApp/meteo/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+import geocoder
+import requests
 
-# Create your views here.
+
+def temp_here():
+    """Return the temperature at the current location."""
+    endpoint = 'https://api.open-meteo.com/v1/forecast'
+    location = geocoder.ip('me').latlng
+    api_request = f'{endpoint}?latitude={location[0]}&longitude={location[1]}&hourly=temperature'
+    return requests.get(api_request).json()
