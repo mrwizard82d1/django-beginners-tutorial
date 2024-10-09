99f28799bca97ddc6c9bbe362798c1b82d294efe
Larry Jones <mrwizard82d1@gmail.com>
2024-10-09 17:31:27 -0500

    Use local timezone

--- weatherApp/meteo/views.py
+++ weatherApp/meteo/views.py
@@ -1,3 +1,6 @@
+import datetime
+from zoneinfo import ZoneInfo
+
 import geocoder
 import requests
 
@@ -6,5 +9,8 @@ def temp_here():
     """Return the temperature at the current location."""
     endpoint = 'https://api.open-meteo.com/v1/forecast'
     location = geocoder.ip('me').latlng
-    api_request = f'{endpoint}?latitude={location[0]}&longitude={location[1]}&hourly=temperature'
-    return requests.get(api_request).json()
+    api_request = f'{endpoint}?latitude={location[0]}&longitude={location[1]}&hourly=temperature_2m'
+    now = datetime.datetime.now(ZoneInfo('America/Chicago'))
+    hour = now.hour
+    meteo_data = requests.get(api_request).json()
+    return meteo_data['hourly']['temperature_2m'][hour]
